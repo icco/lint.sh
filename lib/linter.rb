dd213a15263db5b5bdcb3f1d3fab853d42bda8c9
Nat Welch <nat@natwelch.com>
2014-03-16 10:30:44 -0700

    trying lib

--- lib/linter.rb
+++ lib/linter.rb
@@ -1,3 +1,15 @@
 class Linter
+  attr_accessor :code
 
+  def initialize
+    @code = ""
+  end
+
+  def analyze
+    cli = Rubocop::CLI.new
+    Padrino.logger.push cli.run
+    if self.code.empty?
+      raise ArgumentError "You need to give the Linter code to Lint"
+    end
+  end
 end
--- test/libs/linter_test.rb
+++ test/libs/linter_test.rb
@@ -1,8 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
-describe "Linter Model" do
+describe "Linter Lib" do
   it 'can construct a new instance' do
     @linter = Linter.new
     refute_nil @linter
+  end
+
+  it 'can analyze some code' do
+    @linter = Linter.new
+    @linter.code = "[1, 2, 3].inject(:+)"
+    refute_nil @linter.analyze
   end
 end
